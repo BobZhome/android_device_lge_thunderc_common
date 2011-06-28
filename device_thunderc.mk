668c06bafc7bb2af41d32d0b05fc17d0ae307f07
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-06-28 12:14:23 -0700

    Cleanup.

--- device_thunderc.mk
+++ device_thunderc.mk
@@ -1,4 +1,5 @@
-#$(call inherit-product, build/target/product/full.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 PRODUCT_LOCALES += mdpi
 
@@ -221,8 +222,7 @@ PRODUCT_COPY_FILES += \
     device/lge/thunderc/files/etc/media_profiles.xml:system/etc/media_profiles.xml \
     device/lge/thunderc/files/mvdalvik.sh:system/etc/init.d/01mvdalvik \
 
-
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, build/target/product/full.mk)
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_thunderc
