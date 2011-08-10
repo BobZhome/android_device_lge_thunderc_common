a05572306324e86f986c226cb43f94d6dfbab45f
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-08-09 22:18:27 -0700

    libaudiopolicy: Choose only one output device w/ STRATEGY_SONIFICATION
    STRATEGY_SONIFICATION signifies that we should force output through
    the main speaker.  STRATEGY_MEDIA_SONIFICATION means we should go
    through the hierarchy and find the right device.  Thing is the
    MSM7k audio bits don't seem to support multiple output devices.
    Soo... let's specify *ONLY* the speaker (and not the speaker plus
    whatever else is plugged in) when we get to the STRATEGY_SONIFICATION
    case.
    
    Closes inferiorhumanorgans/android_device_lge_thunderc#45.
    
    Change-Id: Ifa0503a19e563e31fd4d382771d52ddea14c8429

--- libaudio/AudioPolicyManager.cpp
+++ libaudio/AudioPolicyManager.cpp
@@ -194,7 +194,7 @@ uint32_t AudioPolicyManager::getDeviceForStrategy(routing_strategy strategy, boo
         }
 
         // device is DEVICE_OUT_SPEAKER if we come from case STRATEGY_SONIFICATION, 0 otherwise
-        device |= device2;
+        device = device ? device : device2;
         if (device == 0) {
             LOGE("getDeviceForStrategy() speaker device not found");
         }
