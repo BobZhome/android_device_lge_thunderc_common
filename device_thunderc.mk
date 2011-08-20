7437e37eaa7df185d2cfd1bd4f7e6dfa7389c319
Alex Zepeda <alex@inferiorhumanorgans.com>
2011-08-20 01:30:38 -0700

    MS690: Add version info for bluetooth binary blob.
    Change-Id: I330e18c4e046ffd63d63747af38a41fc00811111

--- device_thunderc.mk
+++ device_thunderc.mk
@@ -261,6 +261,7 @@ ifeq ($(SUB_MODEL),MS690)
     CDMA_GOOGLE_BASE := android-metropcs-us
     CDMA_CARRIER_ALPHA := MetroPCS
     CDMA_CARRIER_NUMERIC := 311660
+    BLUETOOTH_FIRMWARE := BCM4325D1_004.002.004.0285.0288.hcd
 endif
 
 # Bluetooth
