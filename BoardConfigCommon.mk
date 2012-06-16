b8ddb5ecd626a5751cba61177bc76219987d8af7
bobzhome <bobzhome@cheerful.ful>
2012-06-16 19:36:33 -0400

    take 2

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -41,17 +41,16 @@ BOARD_HAVE_FM_RADIO := true
 BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
 
 #Wifi
-BOARD_WPA_SUPPLICANT_DRIVER     := WEXT
-BOARD_WLAN_DEVICE               := bcm4325
 WPA_SUPPLICANT_VERSION          := VER_0_6_X
-HOSTAPD_VERSION                 := VER_0_6_X
+BOARD_WLAN_DEVICE               := bcm4325
+BOARD_WEXT_NO_COMBO_SCAN        := true
+BOARD_WPA_SUPPLICANT_DRIVER     := WEXT
+WIFI_DRIVER_HAS_LGE_SOFTAP      := true
 WIFI_DRIVER_MODULE_PATH         := "/system/lib/modules/wireless.ko"
 WIFI_DRIVER_MODULE_ARG          := "firmware_path=/etc/wl/rtecdc.bin nvram_path=/etc/wl/nvram.txt config_path=/data/misc/wifi/config"
 WIFI_DRIVER_MODULE_NAME         := "wireless"
 WIFI_DRIVER_FW_STA_PATH         := "/system/etc/wl/rtecdc.bin"
 WIFI_DRIVER_FW_AP_PATH          := "/system/etc/wl/rtecdc-apsta.bin"
-WIFI_DRIVER_HAS_LGE_SOFTAP      := true
-BOARD_WEXT_NO_COMBO_SCAN        := true
 
 #Qualcomm Related
 BOARD_USES_QCOM_HARDWARE := true
--- libcamera/Android.mk
+++ libcamera/Android.mk
@@ -5,7 +5,35 @@ LOCAL_PATH := $(call my-dir)
 # When zero we link against libmmcamera; when 1, we dlopen libmmcamera.
 DLOPEN_LIBMMCAMERA:=1
 
-####### CAMERA.THUNDERC #######
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES:= QualcommCameraHardware.cpp
+
+LOCAL_CFLAGS:= -DDLOPEN_LIBMMCAMERA=$(DLOPEN_LIBMMCAMERA)
+
+# Can be raised to 6 to improve framerate, at the cost of allocating
+# more ADSP memory. Use 0xa68000 as pool size in kernel to test
+LOCAL_CFLAGS+= -DNUM_PREVIEW_BUFFERS=2 -D_ANDROID_
+
+LOCAL_C_INCLUDES+= \
+    $(TARGET_OUT_HEADERS)/mm-camera \
+    $(TARGET_OUT_HEADERS)/mm-still/jpeg \
+
+LOCAL_SHARED_LIBRARIES:= libutils libui libcamera_client liblog libcutils
+
+LOCAL_SHARED_LIBRARIES+= libbinder
+ifneq ($(DLOPEN_LIBMMCAMERA),1)
+LOCAL_SHARED_LIBRARIES+= liboemcamera
+else
+LOCAL_SHARED_LIBRARIES+= libdl
+endif
+
+LOCAL_MODULE:= libcamera
+include $(BUILD_SHARED_LIBRARY)
+
 ## Make camera wrapper
 
 include $(CLEAR_VARS)
@@ -13,15 +41,14 @@ include $(CLEAR_VARS)
 LOCAL_C_FLAGS        += -O3
 LOCAL_MODULE_TAGS    := optional
 LOCAL_MODULE_PATH    := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE         := camera.$(TARGET_BOARD_PLATFORM)
 LOCAL_SRC_FILES      := cameraHal.cpp
 LOCAL_PRELINK_MODULE := false
 
-LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera
-
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder libcutils libhardware libcamera libui
 LOCAL_C_INCLUDES       := frameworks/base/services \
                           frameworks/base/include \
                           hardware/libhardware/include \
-                          hardware/qcom/display/libgralloc
+                          hardware
 
-LOCAL_MODULE := camera.thunderc
 include $(BUILD_SHARED_LIBRARY)
